7049315b5844d6221d9857003b203017ab3ff742
Sergey Goryanin <frequenteen@gmail.com>
2022-06-07 22:34:34 +0400

    configurations

--- MdrService/Extensions/MdrServiceExtension.cs
+++ MdrService/Extensions/MdrServiceExtension.cs
@@ -1,6 +1,16 @@
+using MdrService.Interfaces;
+using MdrService.Mapping;
+using MdrService.Services;
+
 namespace MdrService.Extensions;
 
-public class MdrServiceExtension
+public static class MdrServiceExtension
 {
-    
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+    {
+        services.AddScoped<IElasticsearchService, ElasticsearchService>();
+        services.AddScoped<IMdrMapping, MdrMapping>();
+
+        return services;
+    }
 }
