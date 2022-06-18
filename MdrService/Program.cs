d017e53a02925bf028c4c61de0ad900c23828731
Sergey Goryanin <frequenteen@gmail.com>
2022-06-18 12:07:15 +0400

    Update Program.cs

--- MdrService/Program.cs
+++ MdrService/Program.cs
@@ -10,12 +10,10 @@ using Microsoft.OpenApi.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-/*
 builder.Services.Configure<ForwardedHeadersOptions>(options =>
 {
     options.KnownProxies.Add(IPAddress.Parse("51.210.99.16"));
 });
-*/
 
 builder.Services.AddApplicationServices(builder.Configuration);
 
@@ -39,19 +37,17 @@ builder.Services.Configure<KestrelServerOptions>(options =>
     options.AllowSynchronousIO = true;
 });
 
-// builder.WebHost.UseUrls("https://localhost:5270");
+builder.WebHost.UseUrls("http://localhost:5270");
 
 // Docker setting
 // builder.WebHost.UseKestrel(options => {options.Listen(IPAddress.Any, 80);});
 
 var app = builder.Build();
 
-/*
 app.UseForwardedHeaders(new ForwardedHeadersOptions
 {
     ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
 });
-*/
 
 app.UseMiddleware<ExceptionMiddleware>();
 
